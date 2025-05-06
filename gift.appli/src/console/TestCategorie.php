ae17570913420ac2b67fb8fb28d437f2658d6d30
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-06 09:46:48 +0200

    Corrige chemin test categorie

--- gift.appli/src/console/TestCategorie.php
+++ gift.appli/src/console/TestCategorie.php
@@ -4,8 +4,8 @@ namespace gift\appli\console;
 
 use gift\appli\models\Categorie;
 use gift\appli\utils\Eloquent;
-require_once '../vendor/autoload.php';
-Eloquent::init('../conf/gift.db.conf.ini.dist');
+require_once __DIR__.'/../vendor/autoload.php';
+Eloquent::init(__DIR__.'/../conf/gift.db.conf.ini.dist');
 
 $list_res = Categorie::get();
 
