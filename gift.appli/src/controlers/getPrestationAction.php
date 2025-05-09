399610fcf68c036012800c210a96acc3dcc86e1f
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-05-09 11:38:14 +0200

    Create getPrestationAction.php

---
+++ gift.appli/src/controlers/getPrestationAction.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controlers;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use gift\appli\models\Prestation;
+
+class getPrestationAction
+{
+    public function __invoke(Request $rq, Response $rs, array $args) : Response {
+        $id = $args['id'];
+        $prestation = Prestation::find($id);
+        if ($prestation) {
+            $rs->getBody()->write(json_encode($prestation));
+            return $rs->withHeader('Content-Type', 'application/json');
+        } else {
+            $rs->getBody()->write('Prestation not found');
+            return $rs->withStatus(404);
+        }
+    }
+}   
+
+?>
