102273816d56a89d95c3bd47c053cdbe827a32ca
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-05-12 11:17:04 +0200

    Update bootstrap.php

--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -9,6 +9,7 @@ Eloquent::init(__DIR__ . '/gift.db.conf.ini.dist');
 
 $app = AppFactory::create();
 $app->addRoutingMiddleware(true, false, false);
+$app->addErrorMiddleware(true, false, false);
 $app->setBasePath('/giftbox');
 
 $app = (require_once __DIR__ . '/routes.php')($app);
