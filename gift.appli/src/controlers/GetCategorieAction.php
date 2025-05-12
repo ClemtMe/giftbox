402f54fb56295a897e67b87ddf911a5b30eb907a
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-12 11:08:46 +0200

    Catch des erreurs pour les catégories

--- gift.appli/src/controlers/GetCategorieAction.php
+++ gift.appli/src/controlers/GetCategorieAction.php
@@ -12,10 +12,19 @@ class GetCategorieAction
             return $response->withStatus(400);
         }
 
-        $categorie = \gift\appli\models\Categorie::find($id);
+        try {
+            $categorie = \gift\appli\models\Categorie::findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            $html = <<<HTML
+            <h1>Catégorie non trouvée</h1>
+            <p>La catégorie que vous recherchez n'existe pas.</p>
+            <a href="/giftbox/categories">Toutes les catégories</a>
+            HTML;
 
-        if (!$categorie) {
-            return $response->withStatus(404)->write('Catégorie non trouvée');
+            $response->getBody()->write($html);
+            return $response->withStatus(400);
+        } catch (\Exception $e) {
+            return $response->withStatus(500);
         }
 
         $html = <<<HTML
