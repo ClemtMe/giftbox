392659b5464cbb2a5341cdf4cf6aa8d26e39cc0e
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-15 09:56:42 +0200

    Prestations par catégorie

--- gift.appli/src/controlers/GetPrestationByCateIdAction.php
+++ gift.appli/src/controlers/GetPrestationByCateIdAction.php
@@ -23,13 +23,10 @@ class GetPrestationByCateIdAction{
             throw new \Slim\Exception\HttpNotFoundException($request, "Aucune prestation trouvée pour la catégorie $categorie->libelle.");
         }
 
-        $text = "Prestations de la catégorie : {$categorie->libelle}\n";
-        foreach ($prestations as $prestation) {
-            $text .= "- {$prestation->libelle}: {$prestation->description}\n";
-        }
-
-        $response->getBody()->write($text);
-
-        return $response->withHeader('Content-Type', 'text/plain');
+        $view = \Slim\Views\Twig::fromRequest($request);
+        return $view->render($response, 'pages/ViewCategoriePrestations.twig', [
+            'prestations' => $prestations,
+            'categorie' => $categorie,
+        ]);
     }
 }
