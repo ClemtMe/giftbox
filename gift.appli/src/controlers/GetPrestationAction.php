5ed2a1a021da5e3f169d02de34b5de546db44c57
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-15 09:49:02 +0200

    Images et prestation

--- gift.appli/src/controlers/GetPrestationAction.php
+++ gift.appli/src/controlers/GetPrestationAction.php
@@ -15,7 +15,7 @@ class GetPrestationAction
         $prestation = Prestation::find($id);
         if ($prestation) {
             $view = Twig::fromRequest($rq);
-            $img = "/images/img" . $prestation->img;
+            $img = '/images/' . $prestation->img;
             return $view->render($rs, 'pages/ViewPrestation.twig', ['nom' => $prestation->libelle, 'description' => $prestation->description,
                 'unite' => $prestation->unite, 'tarif' => $prestation->tarif, 'img' => $img, 'url' => $prestation->url]);
         } else {
