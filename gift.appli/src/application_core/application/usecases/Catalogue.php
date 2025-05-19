bed445f697830625cd0742bbfdc6e79c1650b65b
Manech <chenamgg@gmail.com>
2025-05-19 12:00:03 +0200

    catalogue

---
+++ gift.appli/src/application_core/application/usecases/Catalogue.php
@@ -0,0 +1,61 @@
+<?php
+namespace gift\appli\application_core\application\usecases;
+
+use gift\appli\application_core\domain\entities\Categorie;
+use gift\appli\application_core\domain\entities\Prestation;
+use gift\appli\application_core\domain\entities\Theme;
+
+class Catalogue implements CatalogueInterface
+{
+
+    public function getCategories(): array{
+        $categories = Categorie::all();
+        return $categories->toArray();
+    }
+
+    public function getCategorieById(int $id): array
+    {
+        try {
+            $categorie = Categorie::findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new \Exception("Categorie introuvable");
+        }
+        return $categorie->toArray();
+    }
+
+    public function getPrestationById(int $id): array
+    {
+        try {
+            $prestation = Categorie::findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new \Exception("Prestation introuvable");
+        }
+        return $prestation->toArray();
+    }
+
+    public function getPrestationsByCategorie(int $categ_id): array
+    {
+        try {
+            $categorie = Categorie::findOrFail($categ_id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new \Exception("Categorie introuvable");
+        }
+        return $categorie->prestations->toArray();
+    }
+
+    public function getThemesCoffrets(): array
+    {
+        $themes = Theme::all();
+        return $themes->toArray();
+    }
+
+    public function getCoffretById(int $id): array
+    {
+        try {
+            $coffret = Categorie::findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            throw new \Exception("Coffret introuvable");
+        }
+        return $coffret->toArray();
+    }
+}
---
+++ gift.appli/src/application_core/application/usecases/CatalogueInterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace gift\appli\application_core\application\usecases;
+Interface CatalogueInterface{
+    public function getCategories(): array;
+    public function getCategorieById(int $id): array;
+    public function getPrestationById(int $id): array;
+    public function getPrestationsByCategorie(int $categ_id): array;
+    public function getThemesCoffrets(): array;
+    public function getCoffretById(int $id): array;
+}
