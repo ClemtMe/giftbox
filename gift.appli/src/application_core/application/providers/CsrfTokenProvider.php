7831513318f35b357ca925a91120136e162c290b
Manech <chenamgg@gmail.com>
2025-05-28 14:52:34 +0200

    token

---
+++ gift.appli/src/application_core/application/providers/CsrfTokenProvider.php
@@ -0,0 +1,21 @@
+<?php
+namespace gift\appli\core\application\providers;
+use PhpParser\Token;
+
+class CsrfTokenProvider{
+    static function generate(){
+        $token = md5(uniqid(mt_rand(), true));
+        $_SESSION['token'] = $token;
+        return $token;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    static function check($token){
+        if($token != $_SESSION['token']){
+            unset($_SESSION['token']);
+            throw new \Exception("Token invalide");
+        }
+    }
+}
