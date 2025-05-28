86e55f51dd83ef865bab1ac6c303f340759eddb7
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-28 15:52:55 +0200

    Provider pour l'authentification

--- gift.appli/src/application_core/application/auth/AuthServiceInterface.php
+++ gift.appli/src/application_core/application/auth/AuthServiceInterface.php
@@ -5,5 +5,6 @@ namespace gift\appli\core\application\auth;
 interface AuthServiceInterface
 {
     public function register(string $username, string $password): string;
-    public function loginByCredential(string $username, string $password): bool;
+    public function loginByCredential(string $username, string $password): string;
+    public function getUserById(string $userId): array;
 }
