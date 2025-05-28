e6b3a78833e69fbdb45f43f1a61439e3f26414da
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-28 14:44:14 +0200

    Possibilité d'ajouter/retirer des prestations sur la box courante en session

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -7,9 +7,9 @@ use gift\appli\webui\actions\GetCoffretTypeAction;
 use gift\appli\webui\actions\GetPrestationAction;
 use gift\appli\webui\actions\GetPrestationByCateIdAction;
 use gift\appli\webui\actions\GetPrestationByCoffretIdAction;
+use gift\appli\webui\actions\SetPresta2BoxAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use \gift\appli\webui\actions\AddPresta2BoxAction;
 
 return function (Slim\App $app) {
     // Page d'accueil
@@ -27,8 +27,8 @@ return function (Slim\App $app) {
     // Une préstation selon un ID passé dans la query string
     $app->get('/prestation[/]', GetPrestationAction::class)->setName('prestation');
 
-    //ajout d'une prestation à la box courante
-    $app->get('/prestation/add[/]', AddPresta2BoxAction::class)->setName('add_prestation_to_box');
+    //met la quantite d'une prestation à la box courante
+    $app->post('/prestation[/]', SetPresta2BoxAction::class)->setName('set_prestation_to_box');
 
     // Les préstations d'une categorie selon un ID
     $app->get('/categorie/{id}/prestations[/]', GetPrestationByCateIdAction::class)->setName('prestations_by_categorie');
