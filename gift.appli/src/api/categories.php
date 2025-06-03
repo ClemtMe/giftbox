73ec7c5f8543ad284af0ef8402857e5fce0128bf
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-06-03 15:18:34 +0200

    Create categories.php

---
+++ gift.appli/src/api/categories.php
@@ -0,0 +1,39 @@
+<?php
+namespace gift\api;
+
+use gift\appli\core\application\usecases\Catalogue;
+use gift\appli\core\application\usecases\CatalogueInterface;
+use gift\appli\core\domain\exceptions\EntityNotFoundException;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
+use gift\appli\core\application\exceptions\ExceptionDatabase;
+
+class Categories {
+
+    private CatalogueInterface $catalogue;
+    public function __construct() {
+        $this->catalogue = new Catalogue();
+    }
+
+    public function __invoke($request, $response, $args){
+        //On tente de récupérer les catégories depuis le modèle
+        try {
+            $categories = $this->catalogue->getCategories();
+        } catch (EntityNotFoundException $e) {
+            throw new HttpNotFoundException($request, $e->getMessage());
+        } catch (ExceptionDatabase $e) {
+            throw new HttpInternalServiceException($request, $e->getMessage());
+        }
+
+        //Transformation des données
+        $data = [ 'type' => 'collection',
+            'count' => count($categories),
+            'categories' => $categories ];
+        $response->getBody()->write(json_encode($data));
+
+        //renvoie des données
+        return
+            $response->withHeader('Content-Type','application/json')
+                ->withStatus(200);
+    }
+}
