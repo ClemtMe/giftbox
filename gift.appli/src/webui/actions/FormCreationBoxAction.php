6f3cc09ce3f04ef9bb65324dd33a51239e41b96b
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-06-04 14:38:00 +0200

    Correction des token csrf

--- gift.appli/src/webui/actions/FormCreationBoxAction.php
+++ gift.appli/src/webui/actions/FormCreationBoxAction.php
@@ -1,18 +1,21 @@
 <?php
 namespace gift\appli\webui\actions;
-use gift\appli\webui\providers\CsrfTokenProvider;
+use gift\appli\webui\providers\SessionCsrfTokenProvider;
+use webui\providers\CsrfTokenProviderInterface;
 
 class FormCreationBoxAction{
 
     private string $template;
+    private CsrfTokenProviderInterface $tokenProvider;
     public function __construct(){
         $this->template = 'pages/ViewFormCreationBox.twig';
+        $this->tokenProvider = new SessionCsrfTokenProvider();
     }
     public function __invoke($request, $response, $args){
-        $tokken = CsrfTokenProvider::generate();
+        $token = $this->tokenProvider->generateCsrf();
         $view = \Slim\Views\Twig::fromRequest($request);
         return $view->render($response, $this->template, [
-            'token' => $tokken
+            'csrf_token' => $token
         ]);
     }
 }
---
+++ gift.appli/src/webui/providers/SessionCsrfTokenProvider.php
@@ -0,0 +1,33 @@
+<?php
+namespace gift\appli\webui\providers;
+
+use gift\appli\webui\exceptions\CsrfException;
+use webui\providers\CsrfTokenProviderInterface;
+
+class SessionCsrfTokenProvider implements CsrfTokenProviderInterface{
+
+    private string $sessionName;
+
+    public function __construct(string $sessionName = 'csrf_token'){
+        $this->sessionName = $sessionName;
+    }
+
+
+    public function generateCsrf(): string
+    {
+        $token = md5(uniqid(mt_rand(), true));
+        $_SESSION[$this->sessionName] = $token;
+        return $token;
+    }
+
+    /**
+     * @throws CsrfException
+     */
+    public function checkCsrf($token): void
+    {
+        if($token != $_SESSION[$this->sessionName]){
+            throw new CsrfException();
+        }
+        unset($_SESSION[$this->sessionName]);
+    }
+}
